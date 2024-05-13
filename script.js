e3ea1510ec8f8124dddf5e38ae76953413314652
Luca-Mariselli <lucamariselli@gmail.com>
2024-05-13 16:36:40 +0200

    M1- scritto titolo a schermo con Vue

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            title: 'Hello VueJS'
+            title: 'Hello VueJS',
+            picsum: `https://picsum.photos/200/300`
         }
+    },
+    
+    methods: {
+
+    },
+
+    mounted(){
+        console.log(`Benvenuto`)
     }
-  }).mount('#app')
+
+}).mount('#app')
